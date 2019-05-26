10758e9689832abc569d08ccaa4cb5ae9d1aec7e
ikhan0877 <ikhan0877@gmail.com>
2019-05-26 14:22:32 +0530

    partial index page

--- index.php
+++ index.php
@@ -42,13 +42,79 @@
     </nav>
     <div class="container-fluid smthg" id="">
         <img src="images/banner-image.jpg" >
+<!--
         <div class="banner-text">
             <img src="images/arena-animation-logo.jpg" height="100" width="100">
             <p> FREE YOUR <br />IMAGINATION
             </p>
         </div>
-        <div class="contact-form">
-        
+-->
+        <div class="contact-form card-1">
+                <p>Career guidance</p>
+                <div class="form-group1">
+                  <label for="usr">Name:</label>
+                  <input type="text" class="form-control1" id="usr" placeholder="Enter Your Name">
+                </div>
+                <div class="form-group1">
+                  <label for="email">Email:</label>
+                  <input type="email" class="form-control1" id="email" placeholder="Enter Your Emailid">
+                </div>
+                <div class="form-group1">
+                  <label for="mobile">Mobile:</label>
+                  <input type="tel" class="form-control1" id="mobile" placeholder="Enter Your Mobileno.">
+                </div>
+                <div class="form-group1">
+                  <label for="sel1">Select list:</label>
+                  <select class="form-control1" id="sel1">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                  </select>
+                </div>
+                <div class="form-group1">
+                    <button type="submit" class="blue-btn">Submit</button>
+                </div>
+        </div>
+    </div>
+    <div class="container-fluid abt-section pb-4" >
+        <div class="row">
+            <div class="col-md-8">
+                <h2 class="text-white text-center pt-4 pb-4">About Arena animation(Bangalore)</h2>
+                <p class="text-white text-center pt-4 pb-4 pl-4 pr-4">Arena Animation is the animation, web designing, VFX & multimedia education brand of Aptech Limited, with over 20 years of experience. The Academy gives practical training in 3D & 2D Animation, Web designing, Visual Effects, Graphic designing, Broadcast and Multimedia around the world. 
+                 <br />
+                <br />
+                More than 400,000 students have been successfully trained. Arena classrooms are equipped with world-class infrastructure such as state-of-the-art technology, practice labs, high-end animation tools, 2D animation facilities, 3D software and art rooms. Students also get to access an extensive reference library to improve their knowledge & skills.
+                The Institute provides various career oriented courses which leads to good job placements in major Indian & international studios like Prime Focus, UTV Movies, Maya Entertainment, Star Plus, Reliance Media Works, and many more. Apart from technical training & project work, the School also conducts job placement workshops, industry visits, and seminars by well-known industry experts.
+                </p>
+                <div class="row ">
+                    <div class="col-md-6 col-sm-12 text-center"><button type="submit" class="white-btn mx-auto">Join us</button></div>
+                    <div class="col-md-6 col-sm-12 text-center"><button type="submit" class="white-btn mx-auto">View Course details</button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container-fluid p-4">
+        <div class="row">
+            <div class="col-md-4 text-center student-section ">
+                <p class="p-4 mt-4">Students Completed Course</p>
+                <h4 class="stu-cnt p-4">1000+</h4>
+            </div>
+            <div class="col-md-4 text-center student-section">
+                <p class="p-4 mt-4">Students Placed</p>
+                <h4 class="stu-cnt p-4">1000+</h4>
+            </div>
+            <div class="col-md-4 text-center student-section">
+                <p class="p-4 mt-4">Currently studying</p>
+                <h4 class="stu-cnt p-4">1000+</h4>
+            </div>
+            
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <button type="submit" class="blue-borderd-btn w-25 mx-auto">Go to Students Work</button>
+            </div>
         </div>
     </div>
 
