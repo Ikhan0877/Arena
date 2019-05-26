4b6bb056ef17fd5d40f9add7e65976622e0ae3bf
ikhan0877 <ikhan0877@gmail.com>
2019-05-26 19:47:48 +0530

    partial index

--- index.php
+++ index.php
@@ -95,6 +95,7 @@
         </div>
     </div>
     
+<!--
     <div class="container-fluid p-4">
         <div class="row">
             <div class="col-md-4 text-center student-section ">
@@ -116,7 +117,83 @@
                 <button type="submit" class="blue-borderd-btn w-25 mx-auto">Go to Students Work</button>
             </div>
         </div>
+        
     </div>
+-->
+    <div class=" course-section">
+            <div class="col-md-12"> 
+                 <div class="coure-section text-center p-5 text-white"> 
+                    <h2 class="p-3">Courses Offered</h2>
+                    <p class="p-3">
+                    Arena animation kammanahalli -India's premeier Media and Entertainment training
+                    institutions offers power-packed courses that help shape a student's creative career. with trailor-made programs catering
+                    to specific domains,ARENA ANIMATION KAMMANHALLI trains students in industry relevant,employment-oriended skills through
+                    the most advanced tools and technologies with the help of expert faculty.
+                    </p>
+                </div>
+            </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row ">
+            <div class="col-md-4 col-lg-4 p-5">
+                <div class="card text-center card-1 border-sm  ">
+                    <h2 class="text-theme p-2 mt-2 mb-2">VFX PRIME</h2>
+                    <img class="card-img-top p-3" src="images/vfx-prime.jpg" alt="Card image">
+                    <div class="card-body course-cont text-theme p-2">VFX Prime offers end-to-end training on <br />the latest and most advanced <br />tools in the industry.</div> 
+                    <div class="card-footer bg-primary text-center text-white">View Full Details</div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 p-5">
+                <div class="card text-center card-1 border-sm ">
+                    <h2 class=" p-2 mt-2 mb-2 text-theme">ANIMATION PRIME</h2>
+                    <img class="card-img-top p-3" src="images/animation-prime-course-in-arena-1200x720.jpg" alt="Card image" />
+                    <div class="card-body course-cont text-theme p-3">It offers complete training in <br />all aspect of animation to make its <br />students capable of employment <br />and progress in the animation industry.</div> 
+                    <div class="card-footer bg-primary text-center text-white">View Full Details</div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 p-5">
+                <div class="card text-center card-1 border-sm ">
+                    <h2 class="text-theme p-2 mt-2 mb-2">VFX FILM MAKING</h2>
+                    <img class="card-img-top p-3" src="images/vfx-film-making.jpg" alt="Card image" />
+                    <div class="card-body course-cont text-theme p-3">The circulum is designed to help you master all the the skills needed to be successful in the VFX filmmaking world.</div> 
+                    <div class="card-footer bg-primary text-center text-white">View Full Details</div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 p-5">
+                <div class="card text-center card-1 border-sm ">
+                    <h2 class="text-theme p-2 mt-2 mb-2">BROADCAST PRIME</h2>
+                    <img class="card-img-top p-3" src="images/broadcast-prime-bangalore.jpg" alt="Card image" />
+                    <div class="card-body course-cont text-theme p-3 ">Broadcast Prime trains students tocreate engaging motion visuals by merging the best graphics, animation, live footage,music, sound, electronics,Interactive and unconventional media tools.</div> 
+                    <div class="card-footer bg-primary text-center text-white">View Full Details</div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 p-5">
+                <div class="card text-center card-1 border-sm ">
+                    <h2 class="text-theme p-2 mt-2 mb-2">GWDD</h2>
+                    <img class="card-img-top p-3" src="images/graphics-web-design.jpg" alt="Card image" />
+                    <div class="card-body course-cont text-theme p-3">Spread across two terms,this program trains you in all aspects of Graphic Design, Web Design as well as Development. Arena Animation also offers programs focused on web Design and Print & Publishing.</div> 
+                    <div class="card-footer bg-primary text-center text-white">View Full Details</div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 p-5">
+                <div class="card text-center card-1 border-sm ">
+                    <h2 class="text-theme p-2 mt-2 mb-2">DIGITAL MARKETING</h2>
+                    <img class="card-img-top p-3" src="images/vfx-prime.jpg" alt="Card image">
+                    <div class="card-body course-cont text-theme p-3">The Digital Advertising Design and Marketing Program combines technology with creativity to trains students in the conceptual,design,advertising,marketing,technical and analytical aspects of communication for the digital space.</div> 
+                    <div class="card-footer bg-primary text-center text-white">View Full Details</div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 p-5">
+                <div class="card text-center card-1 border-sm ">
+                    <h2 class="text-theme p-2 mt-2 mb-2">PRINT & PUBLISHING</h2>
+                    <img class="card-img-top p-3" src="images/vfx-prime.jpg" alt="Card image">
+                    <div class="card-body course-cont text-theme p-3">The Digital Advertising Design and Marketing Program combines technology with creativity to trains students in the conceptual,design,advertising,marketing,technical and analytical aspects of communication for the digital space.</div> 
+                    <div class="card-footer bg-primary text-center text-white">View Full Details</div>
+                </div>
+            </div>
+        </div>
+    </div>
+   
 
     
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
