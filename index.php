56d546eeac037e6176ab7397f1bf6432bac8d253
ikhan0877 <ikhan0877@gmail.com>
2019-05-26 20:43:43 +0530

    index page

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
   <meta name="description" content="Arena Animation Kammanahalli">
   <meta name="author" content="Jobith M Basheer">
   <link rel="icon" media="screen" href="images/arena-animation-icon.ico">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
 <body>
     <nav class="navbar navbar-expand-sm bg-light navbar-light">
@@ -139,7 +140,7 @@
                 <div class="card text-center card-1 border-sm  ">
                     <h2 class="text-theme p-2 mt-2 mb-2">VFX PRIME</h2>
                     <img class="card-img-top p-3" src="images/vfx-prime.jpg" alt="Card image">
-                    <div class="card-body course-cont text-theme p-2">VFX Prime offers end-to-end training on <br />the latest and most advanced <br />tools in the industry.</div> 
+<!--                    <div class="card-body course-cont text-theme p-2">VFX Prime offers end-to-end training on <br />the latest and most advanced <br />tools in the industry.</div> -->
                     <div class="card-footer bg-primary text-center text-white">View Full Details</div>
                 </div>
             </div>
@@ -147,7 +148,7 @@
                 <div class="card text-center card-1 border-sm ">
                     <h2 class=" p-2 mt-2 mb-2 text-theme">ANIMATION PRIME</h2>
                     <img class="card-img-top p-3" src="images/animation-prime-course-in-arena-1200x720.jpg" alt="Card image" />
-                    <div class="card-body course-cont text-theme p-3">It offers complete training in <br />all aspect of animation to make its <br />students capable of employment <br />and progress in the animation industry.</div> 
+<!--                    <div class="card-body course-cont text-theme p-3">It offers complete training in <br />all aspect of animation to make its <br />students capable of employment <br />and progress in the animation industry.</div> -->
                     <div class="card-footer bg-primary text-center text-white">View Full Details</div>
                 </div>
             </div>
@@ -155,7 +156,7 @@
                 <div class="card text-center card-1 border-sm ">
                     <h2 class="text-theme p-2 mt-2 mb-2">VFX FILM MAKING</h2>
                     <img class="card-img-top p-3" src="images/vfx-film-making.jpg" alt="Card image" />
-                    <div class="card-body course-cont text-theme p-3">The circulum is designed to help you master all the the skills needed to be successful in the VFX filmmaking world.</div> 
+<!--                    <div class="card-body course-cont text-theme p-3">The circulum is designed to help you master all the the skills needed to be successful in the VFX filmmaking world.</div> -->
                     <div class="card-footer bg-primary text-center text-white">View Full Details</div>
                 </div>
             </div>
@@ -163,7 +164,7 @@
                 <div class="card text-center card-1 border-sm ">
                     <h2 class="text-theme p-2 mt-2 mb-2">BROADCAST PRIME</h2>
                     <img class="card-img-top p-3" src="images/broadcast-prime-bangalore.jpg" alt="Card image" />
-                    <div class="card-body course-cont text-theme p-3 ">Broadcast Prime trains students tocreate engaging motion visuals by merging the best graphics, animation, live footage,music, sound, electronics,Interactive and unconventional media tools.</div> 
+<!--                    <div class="card-body course-cont text-theme p-3 ">Broadcast Prime trains students tocreate engaging motion visuals by merging the best graphics, animation, live footage,music, sound, electronics,Interactive and unconventional media tools.</div> -->
                     <div class="card-footer bg-primary text-center text-white">View Full Details</div>
                 </div>
             </div>
@@ -171,29 +172,102 @@
                 <div class="card text-center card-1 border-sm ">
                     <h2 class="text-theme p-2 mt-2 mb-2">GWDD</h2>
                     <img class="card-img-top p-3" src="images/graphics-web-design.jpg" alt="Card image" />
-                    <div class="card-body course-cont text-theme p-3">Spread across two terms,this program trains you in all aspects of Graphic Design, Web Design as well as Development. Arena Animation also offers programs focused on web Design and Print & Publishing.</div> 
+<!--                    <div class="card-body course-cont text-theme p-3">Spread across two terms,this program trains you in all aspects of Graphic Design, Web Design as well as Development. Arena Animation also offers programs focused on web Design and Print & Publishing.</div> -->
                     <div class="card-footer bg-primary text-center text-white">View Full Details</div>
                 </div>
             </div>
             <div class="col-md-4 col-lg-4 p-5">
                 <div class="card text-center card-1 border-sm ">
                     <h2 class="text-theme p-2 mt-2 mb-2">DIGITAL MARKETING</h2>
-                    <img class="card-img-top p-3" src="images/vfx-prime.jpg" alt="Card image">
-                    <div class="card-body course-cont text-theme p-3">The Digital Advertising Design and Marketing Program combines technology with creativity to trains students in the conceptual,design,advertising,marketing,technical and analytical aspects of communication for the digital space.</div> 
+                    <img class="card-img-top p-3" src="images/digital-advertising.jpg"  alt="Card image">
+<!--                    <div class="card-body course-cont text-theme p-3">The Digital Advertising Design and Marketing Program combines technology with creativity to trains students in the conceptual,design,advertising,marketing,technical and analytical aspects of communication for the digital space.</div> -->
                     <div class="card-footer bg-primary text-center text-white">View Full Details</div>
                 </div>
             </div>
             <div class="col-md-4 col-lg-4 p-5">
                 <div class="card text-center card-1 border-sm ">
                     <h2 class="text-theme p-2 mt-2 mb-2">PRINT & PUBLISHING</h2>
-                    <img class="card-img-top p-3" src="images/vfx-prime.jpg" alt="Card image">
-                    <div class="card-body course-cont text-theme p-3">The Digital Advertising Design and Marketing Program combines technology with creativity to trains students in the conceptual,design,advertising,marketing,technical and analytical aspects of communication for the digital space.</div> 
+                    <img class="card-img-top p-3" src="images/print-publishing.jpg" alt="Card image">
+               
+                    <div class="card-footer bg-primary text-center text-white">View Full Details</div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 p-5">
+                <div class="card text-center card-1 border-sm ">
+                    <h2 class="text-theme p-2 mt-2 mb-2">WEB DESIGNING</h2>
+                    <img class="card-img-top p-3" src="images/web-design.png" alt="Card image">
+               
+                    <div class="card-footer bg-primary text-center text-white">View Full Details</div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4 p-5">
+                <div class="card text-center card-1 border-sm ">
+                    <h2 class="text-theme p-2 mt-2 mb-2">UI/UX DESIGN</h2>
+                    <img class="card-img-top p-3" src="images/ui-ux-design.jpg" alt="Card image">
+               
                     <div class="card-footer bg-primary text-center text-white">View Full Details</div>
                 </div>
             </div>
         </div>
     </div>
-   
+    
+    <!--courses offered end -->
+    <div class="container-fluid why-section p-5"> 
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <h2 class="p-5 text-white ">Why Arena Animation???</h2>
+                <a href="" class="blue-btn">Visit Us</a>
+            </div>
+            <div class="col-md-9 text-center">
+                <div class="row">
+                    <div class="col-md-4 p-5 why-icon">
+                        <img src="images/classroom.png" />
+                        <p>High end Studio</p>
+                    </div>
+                    <div class="col-md-4 p-5 why-icon">
+                        <img src="images/projects.png" />
+                        <p>projects</p>
+                    </div>
+                    <div class="col-md-4 p-5 why-icon">
+                        <img src="images/job%20(1).png" width="75" height="75" />
+                        <p>Job assistance</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 p-5 why-icon">
+                        <img src="images/tools.png" />
+                        <p>Hands-on training using in-demand tools & techniques</p>
+                    </div>
+                    <div class="col-md-4 p-5 why-icon">
+                        <img src="images/classroom.png" />
+                        <p>Guidance to develop your professional portfolio & demo reel</p>
+                    </div>
+                    <div class="col-md-4 p-5 why-icon">
+                        <img src="images/portal.png" />
+                        <p>Access to Onlinevarsity, a learning app to study on-the-go</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!--Testimonial -->
+   <div class="container-fluid testimonial text-white">
+       
+        <div class="row">
+            <div class="col-md-8 text-center p-5 mt-5"> 
+                <h3 class="p-3 mt-5">Mohamed Imran khan</h3>
+                <p class="m-2">Web desiner</p>
+                <p class="m-2">arena animation is a wonderful institude.
+                </p>
+            </div>
+            <div class="col-md-4 text-center p-5">
+                <h3 class="text-center p-5">Testimonial</h3>
+                <i class="fas fa-comment-dots"></i>
+                <a href="" class="white-btn text-center mx-auto">Give Us Your feedback</a>
+            </div>
+        </div>
+   </div>
 
     
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
