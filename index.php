7865ae48df1249db604bc29cb8b691e248ead873
ikhan0877@gmail.com <ikhan0877@gmail.com>
2019-05-27 14:59:50 +0530

    index page completed

--- index.php
+++ index.php
@@ -109,7 +109,7 @@
                 </div>
             </div>
     </div>
-    <div class="container-fluid">
+    <div class="container-fluid course-card">
         <div class="row ">
             <div class="col-md-4 col-lg-4 p-5">
                 <div class="card text-center card-1 border-sm  ">
@@ -267,7 +267,42 @@
         </div>
    </div>
     <footer class="container-fluid">
-        
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <img class="img-fluid mt-5 " width="200" height="200" src="images/arena-animation-logo.jpg">
+                <p class="text-white mt-5">Arena Animation is the leader in Animation Multimedia education with over 20 years of experience, backed by excellent faculty for latest education tools.</p>
+            </div>
+            <div class="col-md-3 text-center text-white">
+                <h3 class="mt-5">Navigations</h3>
+                <ul class="mt-5">
+                    <li class="list-unstyled mt-2">Home</li>
+                    <li class="list-unstyled mt-2">Course</li>
+                    <li class="list-unstyled mt-2">Students Work</li>
+                    <li class="list-unstyled mt-2">Testimonial</li>
+                    <li class="list-unstyled mt-2">Events</li>
+                </ul>
+            </div>
+            <div class="col-md-3 text-center text-white">
+                <h3 class="mt-5">Contact Information</h3>
+                <p class="mt-5 p-5"> 
+                    # 436, 4th Cross,
+                    Kalyan Nagar HRBR Layout, 
+                    2nd Block, Bangalore - 43
+                </p>
+                <i class="fab fa-facebook-square text-primary p-2" style="font-size:25px;"></i>
+                <i class="fab fa-instagram p-2"style="font-size:25px;"></i>
+                <i class="fab fa-youtube text-danger p-2"style="font-size:25px;"></i>
+                <i class="fab fa-twitter text-primary p-2"style="font-size:25px;"></i>
+            </div>
+             <div class="col-md-3 text-center text-white">
+                <h3 class="mt-5 rounded-circle mx-auto" ><i class="fas fa-headset p-3" style="font-size:60px;"></i></h3>
+                 <p>080-25423786 / 9844586786</p>
+                 <h3 class="mt-5"><i class="fas fa-envelope-square p-3" style="font-size:60px;"></i></h3>
+                 <p>admin@arenakamanahalli.in<br>
+                     arenabanaswadi@gmail.com
+                 </p>
+            </div>
+        </div>
     </footer>
     
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
